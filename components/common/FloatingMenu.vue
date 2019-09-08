4ddb7cd921524778bb948b411cc0a55f415ef0b0
ramononl <>
2019-09-08 16:57:06 +0200

    change mobile menu

--- components/common/FloatingMenu.vue
+++ components/common/FloatingMenu.vue
@@ -13,27 +13,27 @@
       </div>
       <div v-if="menuOpen" class="menu-open w-52 mt-4 bg-gray-100 shadow-lg rounded overflow-y-auto flex-shrink transition-all transition-100 transition-ease-in-out">
         <nav>
-          <nuxt-link :to="localePath('index')" :class="[active === 'home' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 sm:text-sm">{{ $t('pages.home') }}</nuxt-link>
-          <nuxt-link :to="localePath({ name: 'index', hash: '#chapter'})" v-scroll-to="'#chapter'" class="bg-gray-100 hover:bg-gray-300 block px-4 py-3 sm:py-2 sm:text-sm border-t border-gray-400">{{ $t('pages.contents') }}</nuxt-link>
-          <nuxt-link :to="localePath('int')" :class="[active === 'introduction' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 sm:text-sm border-t border-gray-400">{{ $t('pages.introduction') }}</nuxt-link>
+          <nuxt-link :to="localePath('index')" :class="[active === 'home' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 sm:text-sm">{{ $t('pages.home') }}</nuxt-link>
+          <nuxt-link :to="localePath({ name: 'index', hash: '#chapter'})" v-scroll-to="'#chapter'" class="bg-gray-100 hover:bg-gray-300 block px-4 py-2 sm:text-sm border-t border-gray-400">{{ $t('pages.contents') }}</nuxt-link>
+          <nuxt-link :to="localePath('int')" :class="[active === 'introduction' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 sm:text-sm border-t border-gray-400">{{ $t('pages.introduction') }}</nuxt-link>
           <div class="bg-white sm:text-sm">
-            <nuxt-link :to="localePath('mex')" :class="[active === 'mexico' ? 'bg-mexico-600 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 border-t border-gray-400">{{ $t('pages.mexico') }}</nuxt-link>
+            <nuxt-link :to="localePath('mex')" :class="[active === 'mexico' ? 'bg-mexico-600 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 border-t border-gray-400">{{ $t('pages.mexico') }}</nuxt-link>
           </div>
           <div class="bg-white sm:text-sm">
-            <nuxt-link :to="localePath('per')" :class="[active === 'peru' ? 'bg-peru-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 border-t border-gray-400">{{ $t('pages.peru') }}</nuxt-link>
+            <nuxt-link :to="localePath('per')" :class="[active === 'peru' ? 'bg-peru-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 border-t border-gray-400">{{ $t('pages.peru') }}</nuxt-link>
           </div>
           <div class="bg-white sm:text-sm">
-            <nuxt-link :to="localePath('col')" :class="[active === 'colombia' ? 'bg-colombia-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 border-t border-gray-400">{{ $t('pages.colombia') }}</nuxt-link>
+            <nuxt-link :to="localePath('col')" :class="[active === 'colombia' ? 'bg-colombia-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 border-t border-gray-400">{{ $t('pages.colombia') }}</nuxt-link>
           </div>
           <div class="bg-white sm:text-sm">
-            <nuxt-link :to="localePath('bra')" :class="[active === 'brazil' ? 'bg-brazil-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 border-t border-gray-400">{{ $t('pages.brazil') }}</nuxt-link>
+            <nuxt-link :to="localePath('bra')" :class="[active === 'brazil' ? 'bg-brazil-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 border-t border-gray-400">{{ $t('pages.brazil') }}</nuxt-link>
           </div>
           <div class="bg-white sm:text-sm">
-            <nuxt-link :to="localePath('arg')" :class="[active === 'argentina' ? 'bg-argentina-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 border-t border-gray-400">{{ $t('pages.argentina') }}</nuxt-link>
+            <nuxt-link :to="localePath('arg')" :class="[active === 'argentina' ? 'bg-argentina-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 border-t border-gray-400">{{ $t('pages.argentina') }}</nuxt-link>
           </div>
-          <nuxt-link :to="localePath('conc')" :class="[active === 'conclusion' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 border-t border-gray-400 sm:text-sm">{{ $t('pages.conclusion') }}</nuxt-link>
-          <nuxt-link :to="localePath('rec')" :class="[active === 'recommendations' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 border-t border-gray-400 sm:text-sm">{{ $t('pages.recommendations') }}</nuxt-link>
-          <nuxt-link :to="localePath('dia')" :class="[active === 'journal' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-3 sm:py-2 border-t border-gray-400 sm:text-sm">{{ $t('pages.journal') }}</nuxt-link>
+          <nuxt-link :to="localePath('conc')" :class="[active === 'conclusion' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 border-t border-gray-400 sm:text-sm">{{ $t('pages.conclusion') }}</nuxt-link>
+          <nuxt-link :to="localePath('rec')" :class="[active === 'recommendations' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 border-t border-gray-400 sm:text-sm">{{ $t('pages.recommendations') }}</nuxt-link>
+          <nuxt-link :to="localePath('dia')" :class="[active === 'journal' ? 'bg-base-blue-500 text-white font-bold' : 'bg-gray-100 hover:bg-gray-300']" class="block px-4 py-2 border-t border-gray-400 sm:text-sm">{{ $t('pages.journal') }}</nuxt-link>
         </nav>
       </div>
     </div>
